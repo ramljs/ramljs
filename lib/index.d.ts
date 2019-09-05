be831f833cc1f603c09932f8841408a409195fdd
Eray Hanoğlu <erayszn@gmx.net>
2019-09-05 17:40:43 +0300

    Development backup

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,10 @@
-import TypeLibrary from './types/TypeLibrary';
-import {Api, ILoadOptions} from './spec/Api';
-import {IRouterOptions} from './expressmw';
+declare module "ramljs" {
+    import TypeLibrary from './types/TypeLibrary';
+    import {Api, ILoadOptions} from './spec/Api';
+    import {IRouterOptions} from './expressmw';
 
-export {Api, ILoadOptions, TypeLibrary};
+    export {Api, ILoadOptions, TypeLibrary};
 
-export declare function expressRouter(apiDoc: Api, options?: IRouterOptions): Promise<any>;
+    export function expressRouter(apiDoc: Api, options?: IRouterOptions): Promise<any>;
+}
+
